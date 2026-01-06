0da9b9c2ca3e42557fa3d2536b187e50782ad17f
Lect. Çisem YAŞAR <97396158+cyasar34@users.noreply.github.com>
2026-01-06 14:28:07 +0300

    Add files via upload

---
+++ for_dongusu.m
@@ -0,0 +1,10 @@
+% Ekrana 10 kez "Merhaba" yazdýran ve her yazdýrmadan 
+% sonra 1 saniye bekleyen kod bloðunu yazýnýz?
+clear all
+clc
+for i=1:10
+    fprintf('Merhaba\n');
+    pause(1);   
+end
+
+
